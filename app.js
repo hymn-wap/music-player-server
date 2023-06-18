d68e9dacd8ac20b7b0a0b51be8d69df1db15d36a
bigjuncn <446262388@qq.com>
2023-06-17 20:52:18 -0500

    Bruce06172052

--- app.js
+++ app.js
@@ -1,16 +1,101 @@
 const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
 
 dotenv.config();
-
+//cors
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: true }))
+    .use(function (req, res, next) {
+        //跨域处理
+        res.setHeader('Access-Control-Allow-Origin', '*');  //允许任何源
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', '*');   //允许任何类型
+        // res.writeHead(200, {"Content-Type": "text/plain;charset=utf-8"});
+        next();  //next 方法就是一个递归调用
+    });
+
+app.use(cors());
+
 
 // middleware
 
 
 
-//test test
+//Bruce
+
+// 假设我们在内存中存储用户信息
+let users = {
+    'user1': '123',
+    'user2': '123'
+};
+
+app.post('/login', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    // 验证用户名和密码
+    if (users[username] === password) {
+        // 签署 token
+        let token = jwt.sign({username: username}, 'your-encryption-secret', {expiresIn: '1h'});
+        res.status(200).json({token: token});
+    } else {
+        res.status(401).send('Error: Invalid username or password');
+    }
+});
+
+app.get('/secure-endpoint', verifyToken, (req, res) => {
+    // 这是一个需要验证的端点
+    res.send('You have access to this secure endpoint');
+    console.log("11199911");
+});
+
+
+
+
+
+
+
+
+// token 验证函数
+function verifyToken(req, res, next) {
+    let token = req.headers['authorization'];
+
+    if (token) {
+        jwt.verify(token, 'your-encryption-secret', (err, data) => {
+            if (err) {
+                res.sendStatus(403);
+            } else {
+                req.userData = data;
+                next();
+            }
+        });
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // listen
